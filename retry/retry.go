53ac267075c1f99dda84045b20619f0fc4458cb3
kooksee <kooksee@163.com>
2020-04-13 18:18:31 +0800

    fix: update retry

--- retry/retry.go
+++ retry/retry.go
@@ -3,13 +3,14 @@ package retry
 import (
 	"github.com/pubgo/g/pkg/randutil"
 	"github.com/pubgo/g/xtry"
-	log "github.com/sirupsen/logrus"
+	"log"
 	"math"
 	"time"
 )
 
 type retry struct {
 	attempt        uint
+	factor         time.Duration
 	delay          time.Duration
 	deadline       time.Time
 	handle         func(uint, time.Duration)
@@ -17,8 +18,8 @@ type retry struct {
 	transformation Transformation
 }
 
-func Retry(handle ...interface{}) error {
-	_r := &retry{}
+func Retry(factor time.Duration, handle ...interface{}) error {
+	_r := &retry{factor: factor}
 	for _, h := range handle {
 		switch hdl := h.(type) {
 		case time.Duration:
@@ -42,47 +43,61 @@ func Retry(handle ...interface{}) error {
 	return _r.Do()
 }
 
-func WithConstant(factor time.Duration) *retry {
+func With(factor time.Duration) *retry {
 	return &retry{
-		strategy: func(uint) time.Duration {
-			return factor
-		},
+		factor: factor,
 	}
 }
 
-func WithRandom(factor time.Duration) *retry {
-	return &retry{
-		strategy: func(uint) time.Duration {
-			return time.Duration(randutil.Uint64n(uint64(factor)))
-		},
+func Every(factor time.Duration) *retry {
+	return With(factor).Constant()
+}
+
+func (t *retry) Constant() *retry {
+	t.strategy = func(u uint) time.Duration {
+		return t.factor
 	}
+	return t
 }
 
-func WithIncremental(initial, increment time.Duration) *retry {
-	return &retry{
-		strategy: func(attempt uint) time.Duration {
-			return initial + (increment * time.Duration(attempt))
-		},
+// Random 随机时间
+func (t *retry) Random() *retry {
+	t.strategy = func(u uint) time.Duration {
+		return time.Duration(randutil.Uint64n(uint64(t.factor)))
 	}
+	return t
 }
 
-func WithLinear(factor time.Duration) *retry {
-	return WithIncremental(0, factor)
-}
-
-func WithExponential(factor time.Duration, base float64) *retry {
-	return &retry{
-		strategy: func(attempt uint) time.Duration {
-			return factor * time.Duration(math.Pow(base, float64(attempt)))
-		},
+func (t *retry) RandomIncremental() *retry {
+	t.strategy = func(u uint) time.Duration {
+		return time.Duration(randutil.Uint64n(uint64(t.factor) * uint64(u)))
 	}
+	return t
 }
 
-func WithBinaryExponential(factor time.Duration) *retry {
-	return WithExponential(factor, 2)
+// Incremental 线性递增
+func (t *retry) Incremental() *retry {
+	t.strategy = func(attempt uint) time.Duration {
+		return t.factor * time.Duration(attempt)
+	}
+	return t
 }
 
-func WithFibonacci(factor time.Duration) *retry {
+// Exponential 指数递增
+func (t *retry) Exponential(base float64) *retry {
+	t.strategy = func(attempt uint) time.Duration {
+		return t.factor * time.Duration(math.Pow(base, float64(attempt)))
+	}
+	return t
+}
+
+// BinaryExponential 二进制指数递增
+func (t *retry) BinaryExponential() *retry {
+	return t.Exponential(2)
+}
+
+// Fibonacci 斐波那契递增
+func (t *retry) Fibonacci() *retry {
 	fibonacci := func() func() int {
 		a, b := 1, 1
 		return func() int {
@@ -90,13 +105,18 @@ func WithFibonacci(factor time.Duration) *retry {
 			return a
 		}
 	}()
-	return &retry{
-		strategy: func(_ uint) time.Duration {
-			return factor * time.Duration(fibonacci())
-		},
+	t.strategy = func(_ uint) time.Duration {
+		return t.factor * time.Duration(fibonacci())
 	}
+	return t
 }
 
+// Go 运行
+func (t *retry) Go() (err error) {
+	return t.Do()
+}
+
+// Do 运行
 func (t *retry) Do() (err error) {
 	if t.delay > 0 {
 		time.Sleep(t.delay)
@@ -106,20 +126,17 @@ func (t *retry) Do() (err error) {
 		t.attempt = math.MaxInt32
 	}
 
-	if t.transformation == nil {
-		t.transformation = func(duration time.Duration) time.Duration {
-			return duration
-		}
-	}
-
 	if t.handle == nil {
 		log.Fatalln("handle func is nil")
 	}
 
 	var dur time.Duration
-	for i := uint(0); i < t.attempt && t.deadline.Before(time.Now()); i++ {
+	for i := uint(1); i <= t.attempt && t.deadline.Before(time.Now()); i++ {
 		if err = xtry.Try(func() {
-			dur = t.transformation(t.strategy(i))
+			if t.transformation != nil {
+				dur = t.transformation(t.strategy(i))
+			}
+
 			t.handle(i, dur)
 		})()(); err == nil {
 			return
@@ -129,27 +146,32 @@ func (t *retry) Do() (err error) {
 	return
 }
 
-func (t *retry) WithDeadline(deadline time.Time) *retry {
+// Deadline 重试截止日期
+func (t *retry) Deadline(deadline time.Time) *retry {
 	t.deadline = deadline
 	return t
 }
 
-func (t *retry) WithAttempt(attempt uint) *retry {
+// Attempt 尝试时间
+func (t *retry) Attempt(attempt uint) *retry {
 	t.attempt = attempt
 	return t
 }
 
-func (t *retry) WithHandle(handle func(attempt uint, dur time.Duration)) *retry {
+// Handle 逻辑业务函数
+func (t *retry) Handle(handle func(attempt uint, dur time.Duration)) *retry {
 	t.handle = handle
 	return t
 }
 
-func (t *retry) WithDelay(dur time.Duration) *retry {
+// Delay 执行handle之前的延长时间
+func (t *retry) Delay(dur time.Duration) *retry {
 	t.delay = dur
 	return t
 }
 
-func (t *retry) WithTransformation(tf Transformation) *retry {
+// Transformation 等待时间转换函数
+func (t *retry) Transformation(tf Transformation) *retry {
 	t.transformation = tf
 	return t
 }
